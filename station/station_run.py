57040d4e80e55557def2157c6b7cc4b7177f4ba7
Harold Ollivier <harold.ollivier@mines.org>
2020-09-23 07:27:24 +0100

    sample code pushing data to local db

--- station/station_run.py
+++ station/station_run.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import sqlite3
+from datetime import datetime
+import time
+import SI1145.SI1145 as SI1145
+
 
 #conn = sqlite3.connect('./measurements.db')
 
@@ -12,4 +16,42 @@ db_file = db_dir / 'measurements.db'
 if not db_dir.exists() or not db_dir.is_dir():
 	db_dir.mkdir()
 
-db = sqlite3.connect(db_file)
+db_conn = sqlite3.connect(db_file)
+db = db_conn.cursor()
+
+# db.execute('''
+# CREATE TABLE measurements
+# (timestamp text, value text, unit text, kind text, comment text)
+# ''')
+
+# UV sensor setup 
+sensor = SI1145.SI1145(busnum=1)
+
+# Data acquisition
+
+print('Press Cntrl + Z to cancel')
+
+while True:
+        timestamp = str(datetime.now())
+        vis = sensor.readVisible()
+        IR = sensor.readIR()
+        UV = sensor.readUV()
+        uvIndex = UV 
+        print('Vis:             ', str(vis))
+        print('IR:              ', str(IR))
+        print('UV:              ', str(uvIndex))
+
+        # db.execute("INSERT INTO measurements VALUES (timestamp, str(vis), 'AU', 'Visible light intensity', '-')")
+        # db.execute("INSERT INTO measurements VALUES (timestamp, str(IR), 'AU', 'IR light intensity', '-')")
+        # db.execute("INSERT INTO measurements VALUES (timestamp, str(uvIndex), 'AU', 'UV index', '-')")
+
+
+        si1145data = [
+                (timestamp, str(vis), 'AU', 'Visible light intensity', '-'),
+                (timestamp, str(IR), 'AU', 'IR light intensity', '-'),
+                (timestamp, str(uvIndex), 'AU', 'UV index', '-')]
+
+        db.executemany('INSERT INTO measurements VALUES (?,?,?,?,?)', si1145data)
+        db_conn.commit()
+        
+        time.sleep(1)
