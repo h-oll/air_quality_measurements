b20ac7f8bec0948109e02e1de10a2337c106cf45
Harold Ollivier <harold.ollivier@mines.org>
2020-10-04 15:18:37 +0200

    added code for postgresql backend

--- station/station_run.py
+++ station/station_run.py
@@ -1,29 +1,59 @@
-from pathlib import Path
-import sqlite3
+#from pathlib import Path
+#import sqlite3
+
+import psycopg2
+import psycopg2.extras
+psycopg2.extras.register_uuid()
+from config import config
+
+import uuid
 from datetime import datetime
 import time
 import SI1145.SI1145 as SI1145
 import bme680
 
 
-#conn = sqlite3.connect('./measurements.db')
+# #conn = sqlite3.connect('./measurements.db')
 
-# looking for db
+# # looking for db
 
-current_dir = Path('.')
-db_dir = current_dir / 'db'
-db_file = db_dir / 'measurements.db'
+# current_dir = Path('.')
+# db_dir = current_dir / 'db'
+# db_file = db_dir / 'measurements.db'
 
-if not db_dir.exists() or not db_dir.is_dir():
-	db_dir.mkdir()
+# if not db_dir.exists() or not db_dir.is_dir():
+# 	db_dir.mkdir()
 
-db_conn = sqlite3.connect(db_file)
-db = db_conn.cursor()
+# db_conn = sqlite3.connect(db_file)
+# db = db_conn.cursor()
 
-# db.execute('''
-# CREATE TABLE measurements
-# (timestamp text, value text, unit text, kind text, comment text)
-# ''')
+# # db.execute('''
+# # CREATE TABLE measurements
+# # (timestamp text, value text, unit text, kind text, comment text)
+# # ''')
+
+def insert_observations(observations):
+    """ insert new observations """
+    sql = """INSERT INTO observations(id, time, value, unit) VALUES(%s, %s, %s, %s)"""
+    conn = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.executemany(sql, observations)
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 # UV sensor setup 
 uv_sensor = SI1145.SI1145(busnum=1)
@@ -63,15 +93,13 @@ while True:
         # db.execute("INSERT INTO measurements VALUES (timestamp, str(IR), 'AU', 'IR light intensity', '-')")
         # db.execute("INSERT INTO measurements VALUES (timestamp, str(uvIndex), 'AU', 'UV index', '-')")
 
-
         si1145data = [
-                (timestamp, str(vis), 'AU', 'Visible light intensity', '-'),
-                (timestamp, str(IR), 'AU', 'IR light intensity', '-'),
-                (timestamp, str(uvIndex), 'AU', 'UV index', '-')]
+                (uuid.uuid4(), timestamp, vis, 'AU - Visible light intensity'),
+                (uuid.uuid4(), timestamp, IR, 'AU - IR light intensity'),
+                (uuid.uuid4(), timestamp, uvIndex, 'AU - UV index')]
 
-        db.executemany('INSERT INTO measurements VALUES (?,?,?,?,?)', si1145data)
-        db_conn.commit()
-
+        insert_observations(si1145data)
+        
         if aq_sensor.get_sensor_data():
                 output = '{0:.2f} C | {1:.2f} hPa | {2:.2f} %RH'.format(
                         aq_sensor.data.temperature,
@@ -84,20 +112,28 @@ while True:
                                 aq_sensor.data.gas_resistance))
 
                         bme680data = [
-                                (timestamp, str(aq_sensor.data.temperature), 'C', 'Temperature', '-'),
-                                (timestamp, str(aq_sensor.data.pressure), 'hPa', 'Pressure', '-'),
-                                (timestamp, str(aq_sensor.data.humidity), '%', 'Relative Humidity', '-'),
-                                (timestamp, str(aq_sensor.data.gas_resistance), 'Ohm', 'Gas resistance', '-')]
+                                (uuid.uuid4(), timestamp, aq_sensor.data.temperature, 'C - Temperature'),
+                                (uuid.uuid4(), timestamp, aq_sensor.data.pressure, 'hPa - Pressure'),
+                                (uuid.uuid4(), timestamp, aq_sensor.data.humidity, '% - Relative Humidity'),
+                                (uuid.uuid4(), timestamp, aq_sensor.data.gas_resistance, 'Ohm - Gas resistance')]
+                        insert_observations(bme680data)
                 
                 else:
                         print(output)
                         bme680data = [
-                                (timestamp, str(aq_sensor.data.temperature), 'C', 'Temperature', '-'),
-                                (timestamp, str(aq_sensor.data.pressure), 'hPa', 'Pressure', '-'),
-                                (timestamp, str(aq_sensor.data.humidity), '%', 'Relative Humidity', '-'),
-                                (timestamp, str(aq_sensor.data.gas_resistance), 'Ohm', 'Gas resistance', '-')]
-                
-                db.executemany('INSERT INTO measurements VALUES (?,?,?,?,?)', bme680data)
-                db_conn.commit()
+                                (uuid.uuid4(), timestamp, aq_sensor.data.temperature, 'C - Temperature'),
+                                (uuid.uuid4(), timestamp, aq_sensor.data.pressure, 'hPa - Pressure'),
+                                (uuid.uuid4(), timestamp, aq_sensor.data.humidity, '% - Relative Humidity'),
+                                (uuid.uuid4(), timestamp, aq_sensor.data.gas_resistance, 'Ohm - Gas resistance')]
+                        insert_observations(bme680data)
+
+        # db.executemany('INSERT INTO measurements VALUES (?,?,?,?,?)', si1145data)
+        # db_conn.commit()
+
+        # db.executemany('INSERT INTO measurements VALUES (?,?,?,?,?)', bme680data)
+        # db_conn.commit()
+
+
         
+
         time.sleep(1)
